d5b6a6985a77fe06c589ea4327e1d239e668b4cc
eanassu <eanassu@gmail.com>
2024-03-27 11:06:12 -0300

    Aula 27/3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "br.fmu.aula"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "br.fmu.aula"
--- app/src/main/java/br/fmu/aula/MainActivity.java
+++ app/src/main/java/br/fmu/aula/MainActivity.java
@@ -2,13 +2,29 @@ package br.fmu.aula;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextNumber;
+    private EditText editTextNummber2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextNumber = findViewById(R.id.editTextNumber);
+        editTextNummber2 = findViewById(R.id.editTextNumber2);
+    }
+
+    public void somar(View view) {
+        int x = Integer.parseInt(editTextNumber.getText().toString());
+        int y = Integer.parseInt(editTextNummber2.getText().toString());
+        int soma = x + y;
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("soma", soma);
+        startActivity(intent);
     }
 }
