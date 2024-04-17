2d0042c6ea87dc0d8dc45cc68491a7027c4396c0
eanassu <eanassu@gmail.com>
2024-04-17 10:31:31 -0300

    Aula 17/4

--- app/src/main/java/br/fmu/aula/DatabaseActivity.java
+++ app/src/main/java/br/fmu/aula/DatabaseActivity.java
@@ -3,18 +3,98 @@ package br.fmu.aula;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatabaseActivity extends AppCompatActivity {
 
+    private EditText editTextRe;
+    private EditText editTextNome;
+    private EditText editTextDataAdmissao;
+    private EditText editTextSalario;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        editTextRe = findViewById(R.id.editTextRe);
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextDataAdmissao = findViewById(R.id.editTextDataAdmissao);
+        editTextSalario = findViewById(R.id.editTextSalario);
+    }
+
+    private void limpar() {
+        editTextRe.getText().clear();
+        editTextNome.getText().clear();
+        editTextDataAdmissao.getText().clear();
+        editTextSalario.getText().clear();
+    }
+
+    public void cadastrar(View view) {
         AppDatabase db = AppDatabase.getInstance(this);
         FuncionarioDao dao = db.funcionarioDao();
-        Funcionario f = new Funcionario(1,"teste1",new Date(), 1000);
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+
+        Date dataAdmissao;
+        try {
+            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e) {
+            dataAdmissao = new Date();
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        Funcionario f = new Funcionario(re,nome,dataAdmissao, salario);
         dao.insert(f);
+        limpar();
+    }
+
+    public void buscarPeloRe(View view) {
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        Funcionario funcionario = dao.buscarPeloRe(re);
+        editTextNome.setText(funcionario.getNome());
+        editTextDataAdmissao.setText(dateFormat.format(funcionario.getDataAdmissao()));
+        editTextSalario.setText(Double.toString(funcionario.getSalario()));
+    }
+
+    public void excluir(View view) {
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+
+        Date dataAdmissao;
+        try {
+            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e) {
+            dataAdmissao = new Date();
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        Funcionario f = new Funcionario(re,nome,dataAdmissao, salario);
+        dao.delete(f);
+        limpar();
+    }
+
+    public void atualizar( View view ) {
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+
+        Date dataAdmissao;
+        try {
+            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e) {
+            dataAdmissao = new Date();
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        Funcionario f = new Funcionario(re,nome,dataAdmissao, salario);
+        dao.update(f);
+        limpar();
     }
 }
